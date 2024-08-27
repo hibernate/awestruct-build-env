a4dd3b1a7c5169c7c161c3ce56d4d06d43626991
Yoann Rodière <yoann@hibernate.org>
2024-08-27 15:50:29 +0200

    Fix update-gemfile.sh when using podman-compose

--- update-gemfile.sh
+++ update-gemfile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
-COMPOSE=podman-compose
-if ! which "$COMPOSE" 2>&1 1>/dev/null
+if which "podman-compose" 2>&1 1>/dev/null
 then
+	COMPOSE="podman-compose"
+	COMPOSE_ARGS_FOR_RUN="--in-pod False"
+else
 	COMPOSE="docker compose"
 fi
 
 $COMPOSE build
-$COMPOSE run -u "$UID:$GID" awestruct-build-env bundle update "${@}"
+$COMPOSE $COMPOSE_ARGS_FOR_RUN run -u "$UID:$GID" awestruct-build-env bundle update "${@}"
